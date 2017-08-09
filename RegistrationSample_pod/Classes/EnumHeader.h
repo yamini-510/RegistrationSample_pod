f0811e7ea3edba0aba5ae314638a2c87c5be70d0
yamini-510 <yamini.vellore@gmail.com>
2017-08-09 11:54:40 +0530

    added some files

---
+++ RegistrationSample_pod/Classes/EnumHeader.h
@@ -0,0 +1,21 @@
+//
+//  EnumHeader.h
+//  LoginSample
+//
+//  Created by Thrymr on 08/08/17.
+//  Copyright © 2017 Thrymr. All rights reserved.
+//
+
+#ifndef EnumHeader_h
+#define EnumHeader_h
+
+typedef enum kindOfObject
+{
+    EmailField,
+    PasswordField,
+    PhoneNumber,
+    DateOfBirthField,
+    
+}kindOfObjectMethodNames;
+
+#endif /* EnumHeader_h */
